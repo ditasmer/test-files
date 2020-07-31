f3282b09e02f495b65fb7421f3cbe65faac21e89
ditasmer <ditasmer@gmail.com>
2020-07-31 09:38:54 +0200

    Valida si existe fichero antes de leer

--- fast_read_write_files.php
+++ fast_read_write_files.php
@@ -1,12 +1,16 @@
 <?php
 //lectura rápida del fichero
-$texto = file_get_contents('files/helloworld.txt');
-echo $texto;
+//valida si existe
+if(file_exists('files/helloworld.txt')){
+	$texto = file_get_contents('files/helloworld.txt');
+	echo $texto;
+}
+
 
 $lon = readfile('files/helloworld.txt');
 echo $lon;
 
-//leactura lia a linea en array
+//lectura linia a linea en array
 $arrayLineas = file('files/helloworld.txt');
 
 echo '<br>';
