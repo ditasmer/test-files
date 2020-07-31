ad07d09e31dde601b3bff76296ed82bd686b5058
ditasmer <ditasmer@gmail.com>
2020-07-31 09:35:54 +0200

    Escritura o creacion de fichero

--- fast_read_write_files.php
+++ fast_read_write_files.php
@@ -11,4 +11,10 @@ $arrayLineas = file('files/helloworld.txt');
 
 echo '<br>';
 print_r($arrayLineas);
+
+//escritura rapida en fichero
+$nuevo_contenido = "Adios mundo \n me voy de aqui";
+
+//crea el fichero en caso de que no exista
+file_put_contents('files/byeworld.txt', $nuevo_contenido);
 ?>
