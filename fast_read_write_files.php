871095122307c8ddaa0ac2ebeafd1d7d8a6080db
ditasmer <ditasmer@gmail.com>
2020-07-31 09:33:06 +0200

    Multilinea II

--- fast_read_write_files.php
+++ fast_read_write_files.php
@@ -2,4 +2,13 @@
 //lectura rápida del fichero
 $texto = file_get_contents('files/helloworld.txt');
 echo $texto;
+
+$lon = readfile('files/helloworld.txt');
+echo $lon;
+
+//leactura lia a linea en array
+$arrayLineas = file('files/helloworld.txt');
+
+echo '<br>';
+print_r($arrayLineas);
 ?>
